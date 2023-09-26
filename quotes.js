f9e72cebe6a8cd2a0f51e77483ed1089a427e2f4
Thomas Wardle <twardle6@googlemail.com>
2023-09-26 15:58:52 +0100

    Ticket 2 Done!

--- quotes.js
+++ quotes.js
@@ -2,28 +2,32 @@ import { promises as fs } from "node:fs";
 import path from "node:path";
 import { v4 as uuidv4 } from "uuid";
 
-const quote1 = {
-    id: uuidv4(),
-    quoteText: "Be yourself; everyone else is already taken.",
-    author: "Unknown"
-}
+
 //Test id randomly generated
 //console.log(quote1.id)
 
 const filePath = path.resolve(process.cwd(), "quotes.json");
 
 export async function addQuote(quoteText, author = "Unknown") {
-    const quotes = await fs.readFile("quotes.json");
-    const quotesList = JSON.parse(quotes);
+    
+    const newQuote = {
+    quoteText,
+    author,
+    id: uuidv4(),
+};
+   
+    await fs.writeFile(filePath, JSON.stringify([], null, 2), "utf8")
+    const quoteList = await fs.readFile("quotes.json");
+    const quotes = JSON.parse(quoteList);
     //console log to test
     //console.log(quotesList);
-    quotesList.push(quote1.quoteText, quote1.author);
+    quotes.push(newQuote);
 
-    const JSONData = JSON.stringify(quotesList);
+    const JSONData = JSON.stringify(quotes);
     await fs.writeFile("quotes.json", JSONData);
-    return quote1;
+    return newQuote;
 }
-await addQuote(quote1);
+await addQuote("You must be the change you wish to see in the world.", "Mahatma Gandhi");
 
 export async function getQuotes() {}
 
